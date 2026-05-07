78ac0f848f02c442f3a3fcc9f89c045bd2322ccd
ArlinBM19 <arlinbrionesmorales@gmail.com>
2026-05-06 20:10:55 -0600

    Implement login route and update main message
    Added a login route and improved main response message.

--- auth-service/app.js
+++ auth-service/app.js
@@ -2,10 +2,34 @@ const express = require('express');
 
 const app = express();
 
+const PORT = 3000;
+
+// Middleware para manejar JSON
+app.use(express.json());
+
+// Ruta principal
 app.get('/', (req, res) => {
-    res.send('Auth Service funcionando');
+    res.send('Auth Service funcionando correctamente');
 });
 
-app.listen(3000, () => {
-    console.log('Servidor ejecutándose en puerto 3000');
+// Ruta de prueba login
+app.post('/login', (req, res) => {
+
+    const { email, password } = req.body;
+
+    if(email && password){
+        res.json({
+            message: 'Inicio de sesión exitoso',
+            user: email
+        });
+    } else {
+        res.status(400).json({
+            message: 'Faltan datos'
+        });
+    }
+});
+
+// Iniciar servidor
+app.listen(PORT, () => {
+    console.log(`Servidor Auth Service ejecutándose en puerto ${PORT}`);
 });
